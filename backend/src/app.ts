7a8afe9152779e23f289149551927029eeaeffff
Saurabh Tripathi <saurabhofficial8840@gmail.com>
2025-10-06 18:26:42 +0530

    updated app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,2 +1,40 @@
 import express from "express"
+import cors from "cors";
+const app= express();
+import cookieParser from "cookie-parser"
 
+//parse json data into req.body
+app.use(express.json())
+
+//cors to allow domain from this origin and also thier cookies 
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+//html form data from frontend
+app.use(express.urlencoded({extended:true}))
+
+//this let server static files like images and css ,js
+app.use(express.static("public"))
+
+//cookies setup
+app.use(cookieParser())
+//cors 
+
+//rouesetup
+import UserRouter from "./router/users.routes.js"
+
+
+app.use("/api/auth",UserRouter)
+
+
+
+
+
+
+
+
+
+
+export default app;
