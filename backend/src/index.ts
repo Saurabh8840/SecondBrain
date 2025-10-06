d1369b345ff42f24676a59b5dd04a1d0e0441266
Saurabh Tripathi <saurabhofficial8840@gmail.com>
2025-10-06 18:26:05 +0530

    fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,30 @@
 
 
+import app from "./app";
+import { PrismaClient} from "@prisma/client"
+import dotenv from "dotenv"
+
+
+dotenv.config()
+const prisma=new PrismaClient();
+
+const port:number=parseInt(process.env.PORT||"5000",10)
+
+async function startServer():Promise<void>{
+
+    try {
+        await prisma.$connect();
+        console.log("connected to database")
+    
+        app.listen(port,()=>{
+             console.log(`server running on http://localhost:${port}`)
+        });
+    } catch (error) {
+         console.error("Database connection failed: ",error);
+         await prisma.$disconnect();
+         process.exit(1)
+    }
+    
+}
+
+startServer();
