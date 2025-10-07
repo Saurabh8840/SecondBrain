7b76564c5c769122c6f2fe10fc30002c6a451b69
Saurabh Tripathi <saurabhofficial8840@gmail.com>
2025-10-07 23:53:52 +0530

    added content route

---
+++ backend/src/router/content.route.ts
@@ -0,0 +1,14 @@
+
+import {Router} from "express"
+import { verifyjwt } from "../middleware/auth.middleware";
+import  {addContent,fetchContent,removeContent } from "../controllers/content.controllers";
+
+const router=Router();
+
+router.use(verifyjwt)
+
+router.route("/addContent").post(addContent);
+router.route("/fetchContent").get(fetchContent);
+router.route("/delete/:contentId").delete(removeContent);
+
+export default router
