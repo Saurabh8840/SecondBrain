97b851831a18a2165f580cb86a7d293d7d14f996
Saurabh Tripathi <saurabhofficial8840@gmail.com>
2025-10-09 22:59:02 +0530

    update few lines

--- backend/src/router/users.routes.ts
+++ backend/src/router/users.routes.ts
@@ -4,6 +4,6 @@ import { loginUser, registerUser } from "../controllers/user.controller";
 const router=Router()
 
 router.route("/signup").post(registerUser)
-router.route("/signin").get(loginUser)
+router.route("/signin").post(loginUser)
 
 export default router;
